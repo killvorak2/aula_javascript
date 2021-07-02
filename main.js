e936631263958e35242665343eea3ca5d87d88b3
killvorak2 <marcelo.ceribelli@gmail.com>
2021-07-02 14:56:26 -0300

    treinos com array e dicionário

--- main.js
+++ main.js
@@ -1,15 +1,45 @@
-var nome = "Marcelo Ceribelli";
-var idade = 29;
-var idade2 = 10;
-var frase = "Italia o melhor time do mundo!!";
+
+
+// var lista = ["maça", "pêra", "laranja"];
+// console.log(lista);
+// //adiciona no array
+// lista.push("uva");
+// //remove o último do array
+// console.log(lista);
+// //mostra o tamanho da lista
+// console.log("Nesta lista tem " + lista.length + " itens")
+// lista.pop();
+// console.log(lista);
+// //inverte a ordem de saida do array
+// console.log(lista.reverse());
+// //recuperar somente as strings da lista ou pegar uma posição especifica
+// console.log(lista.toString()[4]);
+// //Join util para alterar o simbolo de separação dos itens do array
+// console.log(lista.join(" => "));
+
+//dicionario no array 
+var fruta = {nome:"maça", cor:"vermelha"};
+console.log(fruta.nome);
+console.log(fruta.cor);
+
+// lista de dicionários no array
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}, {nome:"abacate", cor:"verde"}];
+console.log(frutas);
+console.log(frutas[1].nome);
+console.log(frutas[1].cor);
+
+// var nome = "Marcelo Ceribelli";
+// var idade = 29;
+// var idade2 = 10;
+// var frase = "Italia o melhor time do mundo!!";
 //alert(nome + " tem " + idade + " anos" );
 //alert(idade + idade2);
-console.log(nome);
-console.log(idade + idade2);
-console.log(frase);
-console.log(frase.replace("Italia", "Alemanha"));
-console.log(frase.toLocaleUpperCase());
-console.log(frase.toLocaleLowerCase());
-console.log(frase.endsWith("mundo"));
-console.log(frase.endsWith("!"));
-console.log(frase.length);
+// console.log(nome);
+// console.log(idade + idade2);
+// console.log(frase);
+// console.log(frase.replace("Italia", "Alemanha"));
+// console.log(frase.toLocaleUpperCase());
+// console.log(frase.toLocaleLowerCase());
+// console.log(frase.endsWith("mundo"));
+// console.log(frase.endsWith("!"));
+// console.log(frase.length);
